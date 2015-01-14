e348ce7396b0b2ddce83220d83d8523684c506c0
Crystal Contreras-Grossman & Lee Jorgensen <student@epicodus.com>
2015-01-14 11:07:27 -0800

    Add specs

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,12 +1,32 @@
 class Term
+  @@term_list = []
   define_method(:initialize) do |word, definition|
     @word = word
     @definition = definition
+    @id = @@term_list.length().+(1)
   end
   define_method(:word) do
     @word
   end
   define_method(:definition) do
     @definition
+  end
+  define_singleton_method(:all) do
+    @@term_list
+  end
+  define_method(:save) do
+    @@term_list.push(self)
+  end
+  define_method(:id) do
+    @id
+  end
+  define_singleton_method(:find) do |definition|
+    found_word = nil
+    @@term_list.each() do |word|
+     if word.id().eql?(definition.to_i())
+        found_word = word
+      end
+    end
+    found_word
   end
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -17,5 +17,25 @@ describe(Term) do
       expect(test_word.definition()).to(eq("large"))
     end
   end
-  describe("#all")
+  describe(".all") do
+    it("is empty at first") do
+      expect(Term.all()).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it ("adds a definition to the array of saved words") do
+      test_word = Term.new("water", "wet")
+      test_word.save()
+      expect(Term.all()).to(eq([test_word]))
+    end
+  end
+  describe(".find") do
+     it("is able to find a searched for term") do
+       test_word = Term.new("nut", "food")
+       test_word.save()
+       test_word_1 = Term.new("spinach", "leafy vegetable")
+       test_word_1.save()
+       expect(Term.find(test_word.id())).to(eq(test_word))
+    end
+  end
 end
