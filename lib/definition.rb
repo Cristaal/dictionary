4ec26ee919baac91e6f33a06c04d4b7ddbd17e43
Crystal Contreras-Grossman & Lee Jorgensen <student@epicodus.com>
2015-01-14 16:25:27 -0800

    add class definition

---
+++ lib/definition.rb
@@ -0,0 +1,12 @@
+class Definition
+  define_method(:initialize) do |definition, language|
+    @definition = [definition]
+    @language = [language]
+  end
+  define_method(:definition) do
+    @definition.join()
+  end
+  define_method(:language) do
+    @language.join()
+  end
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,3 +2,12 @@ require('rspec')
 require('dictionary')
 require('definition')
 require('word')
+
+describe(Definition) do
+  describe("#definition") do
+    it("returns the definition entered by the user") do
+    test_definition = Definition.new("my name is earl", "english")
+    expect(test_definition.definition()).to(eq("my name is earl"))
+    end
+  end
+end
